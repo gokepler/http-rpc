a018ad5e836cb039651546946168b648dca42f9f
Wang Jiacheng <wangjiacheng@hsgene.com>
2016-07-08 18:01:43 +0800

    fix null parameter

--- http-rpc-core/src/main/java/io/http/rpc/serialize/ProtoSerializeScheme.java
+++ http-rpc-core/src/main/java/io/http/rpc/serialize/ProtoSerializeScheme.java
@@ -8,11 +8,15 @@ public class ProtoSerializeScheme implements SerializeScheme {
     @Override
     public <T> byte[] serialize(T bean) {
 
+        if(bean == null) { return new byte[0]; }
+
         return ProtoSerializeUtils.encode(bean);
     }
 
     @Override
     public <T> T deserialize(byte[] data, Class<T> clazz) {
+
+        if(data.length == 0) { return null; }
 
         return ProtoSerializeUtils.decode(data, clazz);
     }
